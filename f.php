fa2eb62adf1c50d89f96e00c065c68b013facb67
fizazi <fizazi>
2018-04-06 01:20:48 +0100

    test2

---
+++ f.php
@@ -0,0 +1,18 @@
+< ! D O C T Y P E  h t m l  P U B L I C  " - / / W 3 C / / D T D  X H T M L  1 . 0  S t r i c t / / E N "
+" h t t p : / / w w w . w 3 . o r g / T R / x h t m l 1 / D T D / x h t m l 1 - s t r i c t . d t d " >
+< h t m l  x m l n s = " h t t p : / / w w w . w 3 . o r g / 1 9 9 9 / x h t m l "  x m l : l a n g = " f r "  l a n g = 
+    < h e a d >
+        < t i t l e > N o t r e  p r e m i è r e  i n s t r u c t i o n  :  e c h o < / t i t l e >
+        < m e t a  h t t p - e q u i v = " C o n t e n t - T y p e "  c o n t e n t = " t e x t / h t m l ;
+c h a r s e t = i s o - 8 8 5 9 - 1 "  / >
+    < / h e a d >
+    < b o d y >
+        < h 2 > A f f i c h a g e  d e  t e x t e  a v e c  P H P < / h 2 >
+       
+        < p >
+ C e t t e  l i g n e  a  é t é  é c r i t e  e n t i è r e m e n t  e n  H T M L . < b r  / >
+            < ? p h p  e c h o  " C e l l e - c i  a  é t é  é c r i t e  e n t i è r e m e n t  e n  P H P . " ;
+? >
+        < / p >
+    < / b o d y >
+< / h t m l >
