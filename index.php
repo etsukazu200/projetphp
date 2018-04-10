647df52ba7d1acd01a67703e9386a435d1dac5ef
fizazi <fizazi>
2018-04-10 19:39:47 +0100

    inclure une page ds une autre

---
+++ index.php
@@ -0,0 +1,48 @@
+< ! D O C T Y P E  h t m l >
+< h t m l >
+    < h e a d >
+        < m e t a  c h a r s e t = " u t f - 8 "  / >
+        < t i t l e > M o n  s u p e r  s i t e < / t i t l e >
+    < / h e a d >
+	 < b o d y >
+
+    < ! - -  L ' e n - t ê t e  - - >
+   
+    < h e a d e r >
+      
+    < / h e a d e r >
+   
+    < ! - -  L e  m e n u  - - >
+   
+    < n a v  i d = " m e n u " >        
+        < d i v  c l a s s = " e l e m e n t _ m e n u " >
+            < h 3 > T i t r e  m e n u < / h 3 >
+            < u l >
+                < l i > < a  h r e f = " p a g e 1 . h t m l " > L i e n < / a > < / l i >
+                < l i > < a  h r e f = " p a g e 2 . h t m l " > L i e n < / a > < / l i >
+                < l i > < a  h r e f = " p a g e 3 . h t m l " > L i e n < / a > < / l i >
+            < / u l >
+        < / d i v >    
+    < / n a v >
+   
+    < ! - -  L e  c o r p s  - - >
+   
+    < d i v  i d = " c o r p s " >
+        < h 1 > M o n  s u p e r  s i t e < / h 1 >
+       
+        < p >
+            B i e n v e n u e  s u r  m o n  s u p e r  s i t e  ! < b r  / >
+            V o u s  a l l e z  a d o r e r  i c i ,  c ' e s t  u n  s i t e  g é n i a l  q u i  v a 
+p a r l e r  d e . . .  e u h . . .  J e  c h e r c h e  e n c o r e  u n  p e u  l e  t h è m e  d e  m o n  s i t e . 
+: - D
+        < / p >
+    < / d i v >
+   
+    < ! - -  L e  p i e d  d e  p a g e  - - >
+   
+    < f o o t e r  i d = " p i e d _ d e _ p a g e " >
+        < p > C o p y r i g h t  m o i ,  t o u s  d r o i t s  r é s e r v é s < / p >
+    < / f o o t e r >
+   
+    < / b o d y >
+< / h t m l >
